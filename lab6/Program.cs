c5897e4b8ccab9904949e1980fd149ab4ff80191
Leonid Fiksler <l.fiksler@vkteam.ru>
2024-09-12 18:25:36 +0300

    Started lab 7

---
+++ lab6/Program.cs
@@ -0,0 +1,54 @@
+﻿class Program
+{
+    static int[,] RandomMatrix(int n, int m)
+    {
+        var result = new int[n, m];
+
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = 0; j < m; ++j)
+            {
+                result[i, j] = Random.Shared.Next();
+            }
+        }
+
+        return result;
+    }
+
+    static List<int> RandomEven(int n, int m)
+    {
+        List<int> result = [];
+        var matrix = RandomMatrix(n, m);
+
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = 0; j < m; ++j)
+            {
+                if (matrix[i, j] % 2 == 0)
+                {
+                    result.Add(matrix[i, j]);
+                }
+            }
+        }
+        return result;
+    }
+
+    static int Main()
+    {
+        Console.WriteLine("Input matrix dimensions");
+        var dimensions = Console.ReadLine()?.Split(' ', 2).Select(int.Parse).ToArray();
+        if (dimensions == null)
+        {
+            Console.WriteLine("Error reading");
+            return -1;
+        }
+
+        Func<int, int, List<int>> func = RandomEven;
+
+        var result = func(dimensions[0], dimensions[1]);
+        result.ForEach(elem => Console.Write("{0} ", elem));
+        Console.WriteLine();
+
+        return 0;
+    }
+}
---
+++ lab7/Program.cs
@@ -0,0 +1,30 @@
+﻿class Reflection
+{
+    public string strField = "";
+    public int intField;
+
+    [CustomAttribute]
+    public int intFieldWithAttribute;
+
+    public int Method(string str)
+    {
+        return strField.Length + str.Length;
+    }
+
+    [CustomAttribute]
+    public int MethodWithAttribute(string str)
+    {
+        return Method(str);
+    }
+}
+
+class CustomAttribute : Attribute { }
+
+class Program
+{
+    static int Main()
+    {
+
+        return 0;
+    }
+}
