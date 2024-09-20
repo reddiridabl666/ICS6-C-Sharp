687977f2b0756084bec5e7eb61e68ce85f446375
Leonid Fiksler <reddiridabl666@gmail.com>
2024-09-20 23:21:57 +0300

    Done lab12

---
+++ lab12/MainForm.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace lab12
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.quitBtn = new System.Windows.Forms.Button();
+            this.resetBtn = new System.Windows.Forms.Button();
+            this.Column3 = new System.Windows.Forms.DataGridViewImageColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewImageColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewImageColumn();
+            this.board = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.board)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 18F);
+            this.label1.Location = new System.Drawing.Point(171, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(181, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ход крестиков";
+            // 
+            // quitBtn
+            // 
+            this.quitBtn.Location = new System.Drawing.Point(419, 391);
+            this.quitBtn.Name = "quitBtn";
+            this.quitBtn.Size = new System.Drawing.Size(75, 23);
+            this.quitBtn.TabIndex = 1;
+            this.quitBtn.Text = "Выход";
+            this.quitBtn.UseVisualStyleBackColor = true;
+            this.quitBtn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // resetBtn
+            // 
+            this.resetBtn.Location = new System.Drawing.Point(210, 391);
+            this.resetBtn.Name = "resetBtn";
+            this.resetBtn.Size = new System.Drawing.Size(103, 23);
+            this.resetBtn.TabIndex = 3;
+            this.resetBtn.Text = "Начать заново";
+            this.resetBtn.UseVisualStyleBackColor = true;
+            this.resetBtn.Visible = false;
+            this.resetBtn.Click += new System.EventHandler(this.resetBtn_Click);
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // board
+            // 
+            this.board.AllowUserToAddRows = false;
+            this.board.AllowUserToDeleteRows = false;
+            this.board.AllowUserToResizeColumns = false;
+            this.board.AllowUserToResizeRows = false;
+            this.board.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.board.ColumnHeadersVisible = false;
+            this.board.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            this.board.Location = new System.Drawing.Point(111, 71);
+            this.board.Name = "board";
+            this.board.ReadOnly = true;
+            this.board.RowHeadersVisible = false;
+            this.board.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.board.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.board.Size = new System.Drawing.Size(300, 300);
+            this.board.TabIndex = 2;
+            this.board.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.board_CellContentClick);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(517, 450);
+            this.Controls.Add(this.resetBtn);
+            this.Controls.Add(this.board);
+            this.Controls.Add(this.quitBtn);
+            this.Controls.Add(this.label1);
+            this.Name = "MainForm";
+            this.Text = "Крестики-нолики";
+            ((System.ComponentModel.ISupportInitialize)(this.board)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button quitBtn;
+        private System.Windows.Forms.Button resetBtn;
+        private System.Windows.Forms.DataGridViewImageColumn Column3;
+        private System.Windows.Forms.DataGridViewImageColumn Column2;
+        private System.Windows.Forms.DataGridViewImageColumn Column1;
+        private System.Windows.Forms.DataGridView board;
+    }
+}
+
---
+++ lab12/MainForm.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace lab12
+{
+    public partial class MainForm : Form
+    {
+
+        enum Tile
+        {
+            Blank,
+            Cross,
+            Circle
+        }
+
+        Tile turn;
+        Tile[,] gameState;
+
+        Dictionary<Tile, byte[]> imgs = new Dictionary<Tile, byte[]> {
+            {Tile.Cross, Properties.Resources.cross},
+            {Tile.Circle, Properties.Resources.circle},
+        };
+        
+        Dictionary<Tile, string> turnText = new Dictionary<Tile, string> {
+            {Tile.Cross, "крестиков"},
+            {Tile.Circle, "ноликов"},
+        };
+
+        public MainForm()
+        {
+            InitializeComponent();
+            for (int i = 0; i < 3; ++i)
+            {
+                board.Rows.Add();
+                board.Rows[i].Height = 100;
+            }
+            RestartGame();
+        }
+
+        private void RestartGame()
+        {
+            turn = Tile.Cross;
+            gameState = new Tile[3, 3];
+            resetBtn.Hide();
+
+            for (int i = 0; i < 3; ++i)
+            {
+                for (int j = 0; j < 3; ++j)
+                {
+                    ((DataGridViewImageCell)board.Rows[i].Cells[j]).Value = Properties.Resources.blank;
+                }
+            }
+
+            label1.Text = string.Format("Ход {0}", turnText[turn]);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void board_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (gameState[e.RowIndex, e.ColumnIndex] == Tile.Blank && !resetBtn.Visible)
+            {
+                gameState[e.RowIndex, e.ColumnIndex] = turn;
+                board.Rows[e.RowIndex].Cells[e.ColumnIndex].Value = imgs[turn];
+
+                if (turn == Tile.Cross)
+                {
+                    turn = Tile.Circle;
+                } else
+                {
+                    turn = Tile.Cross;
+                }
+
+                label1.Text = string.Format("Ход {0}", turnText[turn]);
+
+                CheckGameOver();
+            }
+        }
+
+        private void CheckGameOver()
+        {
+            var result = IsGameOverRow();
+            if (result != Tile.Blank)
+            {
+                GameOver(result);
+                return;
+            }
+            
+            result = IsGameOverColumn();
+            if (result != Tile.Blank)
+            {
+                GameOver(result);
+                return;
+            }
+
+            result = IsGameOverDiag();
+            if (result != Tile.Blank)
+            {
+                GameOver(result);
+                return;
+            }
+
+            for (int i = 0; i < 3; ++i)
+            {
+                for (int j = 0; j < 3; ++j)
+                {
+                    if (gameState[i, j] == Tile.Blank)
+                    {
+                        return;
+                    }
+                }
+            }
+
+            GameOver(Tile.Blank);
+        }
+
+        private Tile IsGameOverRow()
+        {
+            uint crossRow = 0;
+            uint circleRow = 0;
+
+            for (int i = 0; i < 3; ++i)
+            {
+                for (int j = 0; j < 3; ++j)
+                {
+                    if (gameState[i, j] == Tile.Cross)
+                    {
+                        ++crossRow;
+                    }
+
+                    if (gameState[i, j] == Tile.Circle)
+                    {
+                        ++circleRow;
+                    }
+                }
+
+                if (crossRow == 3)
+                {
+                    return Tile.Cross;
+
+                }
+
+                if (circleRow == 3)
+                {
+                    return Tile.Circle;
+                }
+
+                crossRow = 0;
+                circleRow = 0;
+            }
+
+            return Tile.Blank;
+        }
+        
+        private Tile IsGameOverColumn()
+        {
+            uint crossColumn = 0;
+            uint circleColumn = 0;
+
+            for (int i = 0; i < 3; ++i)
+            {
+                for (int j = 0; j < 3; ++j)
+                {
+                    if (gameState[j, i] == Tile.Cross)
+                    {
+                        ++crossColumn;
+                    }
+
+                    if (gameState[j, i] == Tile.Circle)
+                    {
+                        ++circleColumn;
+                    }
+                }
+
+                if (crossColumn == 3)
+                {
+                    return Tile.Cross;
+                }
+
+                if (circleColumn == 3)
+                {
+                    return Tile.Circle;
+                }
+
+                crossColumn = 0;
+                circleColumn = 0;
+            }
+
+            return Tile.Blank;
+        }
+
+        private Tile IsGameOverDiag()
+        {
+            if (gameState[0, 0] == gameState[1, 1] && gameState[1, 1] == gameState[2, 2])
+            {
+                return gameState[0, 0];
+            }
+            
+            if (gameState[0, 2] == gameState[1, 1] && gameState[1, 1] == gameState[2, 0])
+            {
+                return gameState[0, 2];
+            }
+
+            return Tile.Blank;
+        }
+
+        private void GameOver(Tile winner)
+        {
+            switch (winner)
+            {
+                case Tile.Cross:
+                    label1.Text = "Победили крестики";
+                    break;
+                case Tile.Circle:
+                    label1.Text = "Победили нолики";
+                    break;
+                default:
+                    label1.Text = "Ничья";
+                    break;
+            }
+
+            resetBtn.Show();
+        }
+
+        private void resetBtn_Click(object sender, EventArgs e)
+        {
+            RestartGame();
+        }
+    }
+}
+ 
